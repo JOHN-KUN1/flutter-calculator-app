0f837af8186a201293c95a188dbdffa530ddfb70
john <elitejhn5@gmail.com>
2025-09-10 17:15:41 +0100

    add calculation logic

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:string_equation/string_equation.dart';
+
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
@@ -10,6 +12,10 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  String userInput = '';
+  String calculatedAnswer = '';
+  String storedAns = '';
+
   final List<String> values = [
     'C',
     'DEL',
@@ -30,66 +36,130 @@ class _CalculatorState extends State<Calculator> {
     '0',
     '.',
     'ANS',
-    '&',
+    '=',
   ];
 
-  Color buttonColorCheck(String val){
-    if (val == 'C'){
+  Color buttonColorCheck(String val) {
+    if (val == 'C') {
       return Colors.green;
-    }
-    else if(val == 'DEL'){
+    } else if (val == 'DEL') {
       return Colors.red;
-    }
-    else if(val == '%' || val == '+' || val == '-' || val =='&' || val == '/' || val == '*'){
+    } else if (val == '%' ||
+        val == '+' ||
+        val == '-' ||
+        val == '=' ||
+        val == '/' ||
+        val == '*') {
       return Colors.blue;
-    }
-    else{
+    } else {
       return Colors.white;
     }
   }
 
-  Color textColorCheck(String val){
-    final blackText = ['1','2','3','4','5','6','7','8','9','0','.','ANS'];
+  Color textColorCheck(String val) {
+    final blackText = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '0',
+      '.',
+      'ANS',
+    ];
 
-    if (blackText.contains(val)){
-      return Colors.black;
-    }
-    else{
+    if (blackText.contains(val)) {
+      return Colors.lightBlue;
+    } else {
       return Colors.white;
     }
+  }
 
+  void onKeyPressed(String val) {
+    if (val == 'DEL'){
+      setState(() {
+        userInput = userInput.substring(0, userInput.length - 1);
+      });
+    }else if(val == '='){
+      if(userInput.isEmpty){
+        return;
+      }else{
+        calculatedAnswer = ConditionEquation().evaluateExpression(userInput).toString();
+      }
+      setState(() {
+        storedAns = calculatedAnswer;
+      });
+    }else if(val == 'C'){
+      setState(() {
+        userInput = '';
+        calculatedAnswer = '';
+      });
+    }else if(val == 'ANS'){
+      setState(() {
+        userInput = storedAns;
+      });
+    }
+    else{
+      setState(() {
+        userInput += val;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.end,
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        const Padding(
-          padding: EdgeInsets.only(right: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    '1',
-                    style: TextStyle(fontSize: 12),
+        Padding(
+          padding: const EdgeInsets.only(left: 4,right: 10),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  userInput,
+                  style: TextStyle(
+                    color: Colors.white.withValues(alpha: 0.6),
+                    fontSize: 30,
                   ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    '7',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
-        const SizedBox(height: 50,),
+        const SizedBox(
+          height: 5,
+        ),
+        Padding(
+          padding:  const EdgeInsets.only(left: 4,right: 10),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  calculatedAnswer,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 44,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
         Flexible(
           child: Padding(
             padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
@@ -97,27 +167,32 @@ class _CalculatorState extends State<Calculator> {
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 4,
                 crossAxisSpacing: 10,
-                mainAxisSpacing: 10
+                mainAxisSpacing: 10,
               ),
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
               itemCount: values.length,
               itemBuilder: (context, index) {
                 return InkWell(
-                  onTap: (){},
+                  onTap: () {
+                    onKeyPressed(values[index]);
+                  },
                   child: Container(
                     decoration: BoxDecoration(
-                      color:  buttonColorCheck(values[index]),
-                      borderRadius: BorderRadius.circular(20)
+                      color: buttonColorCheck(values[index]),
+                      borderRadius: BorderRadius.circular(20),
                     ),
                     height: 30,
                     width: 30,
                     child: Center(
-                      child: Text(values[index], style: TextStyle(
-                        color: textColorCheck(values[index]),
-                        fontWeight: FontWeight.w700,
-                        fontSize: 18
-                      ),),
+                      child: Text(
+                        values[index],
+                        style: TextStyle(
+                          color: textColorCheck(values[index]),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
+                      ),
                     ),
                   ),
                 );
